e672ba5f4f66db1f830552334aaa1ff43606927f
Harryrana98 <ranaharendra77@gmail.com>
2025-05-01 15:44:21 +0530

    updated

---
+++ script.js
@@ -0,0 +1,30 @@
+const form = document.querySelector("form");
+const sourceSelect = document.querySelector("#sourceSelect");
+const targetSelect = document.querySelector("#targetSelect");
+const input = document.querySelector("input");
+
+form.addEventListener("submit", async function (e) {
+  e.preventDefault();
+  const URL = "https://text-translator2.p.rapidapi.com/translate";
+  const data = `?source_language=${sourceSelect.value}&target_language=${targetSelect.value}&text=${input.value}`;
+  const headers = {
+    "x-rapidapi-key": "08c525f37bmsh8a4c5ee2b28fd11p1ed1fdjsn7db831f26e35",
+    "x-rapidapi-host": "google-translator9.p.rapidapi.com",
+    "Content-Type": "application/x-www-form-urlencoded",
+  };
+
+  // const headers = {
+  //     'x-rapidapi-key':'fcc2337bc8msh42ed8779cd4654cp181e15jsn3854dedb9b02',
+  //     'x-rapidapi-host': 'text-translator2.p.rapidapi.com',
+  //     'Content-Type': 'application/x-www-form-urlencoded'
+  // }
+
+  const response = await fetch(URL, {
+    method: "POST",
+    headers: headers,
+    body: data,
+  });
+
+  const result = await response.json();
+  console.log(result);
+});
