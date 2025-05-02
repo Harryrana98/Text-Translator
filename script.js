fad35c2f632721887811a9210826f40d6a1b8f26
Harryrana98 <ranaharendra77@gmail.com>
2025-05-02 16:29:01 +0530

    api language update

--- script.js
+++ script.js
@@ -2,42 +2,64 @@ const form = document.querySelector("form");
 const sourceSelect = document.querySelector("#sourceSelect");
 const targetSelect = document.querySelector("#targetSelect");
 const input = document.querySelector("input");
-const results=document.querySelector(".results")
+const results = document.querySelector(".results");
 
 form.addEventListener("click", async function (e) {
   e.preventDefault();
   const URL = "https://text-translator2.p.rapidapi.com/translate";
   const data = `?source_language=${sourceSelect.value}&target_language=${targetSelect.value}&text=${input.value}`;
-  //   const headers = {
-    //     "x-rapidapi-key": "08c525f37bmsh8a4c5ee2b28fd11p1ed1fdjsn7db831f26e35",
-    //     "x-rapidapi-host": "google-translator9.p.rapidapi.com",
-    //     "Content-Type": "application/x-www-form-urlencoded",
-    //   };
-    
-    const headers = {
-      'x-rapidapi-key':'fcc2337bc8msh42ed8779cd4654cp181e15jsn3854dedb9b02',
-      'x-rapidapi-host': 'text-translator2.p.rapidapi.com',
-      'Content-Type': 'application/x-www-form-urlencoded'
-    }
-    
-    const response = await fetch(URL, {
-      method: "POST",
-      headers: headers,
-      body: data,
-    });
-    
-    const result = await response.json();
-    console.log(result.data);
-    getDisplayData(result.data)
-    results.style.display="block"
-    sourceSelect.value=""
-    targetSelect.value=""
-    input.value=""
+  const headers = {
+    "x-rapidapi-key": "fcc2337bc8msh42ed8779cd4654cp181e15jsn3854dedb9b02",
+    "x-rapidapi-host": "text-translator2.p.rapidapi.com",
+    "Content-Type": "application/x-www-form-urlencoded",
+  };
+
+  const response = await fetch(URL, {
+    method: "POST",
+    headers: headers,
+    body: data,
+  });
+
+  const result = await response.json();
+  console.log(result.data);
+  getDisplayData(result.data);
+  results.style.display = "block";
+  sourceSelect.value = "";
+  targetSelect.value = "";
+  input.value = "";
 });
 
-function getDisplayData(obj){
-  results.innerHTML=""
-    const translateText=document.createElement("h4")
-    translateText.innerText=obj.translatedText
-    results.appendChild(translateText)
+function getDisplayData(obj) {
+  results.innerHTML = "";
+  const translateText = document.createElement("h4");
+  translateText.innerText = obj.translatedText;
+  results.appendChild(translateText);
 }
+
+async function getLanguage() {
+  const LANG_URL = "https://text-translator2.p.rapidapi.com/getLanguages";
+  const headers = {
+    "x-rapidapi-key": "fcc2337bc8msh42ed8779cd4654cp181e15jsn3854dedb9b02",
+    "x-rapidapi-host": "text-translator2.p.rapidapi.com",
+    "Content-Type": "application/x-www-form-urlencoded",
+  };
+
+  const response = await fetch(LANG_URL, {
+    method: "get",
+    headers: headers,
+  });
+  const result = await response.json();
+
+  result.data.languages.forEach((langArr) => {
+    const targetOption = document.createElement("option");
+    const sourceOption = document.createElement("option");
+    sourceOption.innerText = langArr.name;
+    targetOption.innerText = langArr.name;
+    sourceOption.value = langArr.code;
+    targetOption.value = langArr.code;
+
+    sourceSelect.append(sourceOption);
+    targetSelect.append(targetOption);
+  });
+}
+getLanguage();
